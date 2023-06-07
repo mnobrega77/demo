d764f1068a6627defc8d4e58ba9e26abe1f858cd
fcaumartin <fcaumartin@gmail.com>
2023-06-07 08:27:53 +0200

    add form without entity

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Form\DemoFormType;
 use App\Form\ContactFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
@@ -39,6 +40,39 @@ class ContactController extends AbstractController
 
         return $this->render('contact/index.html.twig', [
 //            'form' => $form->createView(),
+              'form' => $form
+        ]);
+    }
+
+
+    #[Route('/contactdemo', name: 'app_contactdemo')]
+    public function index_demo(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Lors de la création du formulaire, nous pouvons spécifier les valeurs initiales des champs
+        $form = $this->createForm(DemoFormType::class, [
+            "objet" => "Entrez un texte !!!",
+            "email" => "",
+            "message" => "",
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On peut récupérer toutes données du formulaire sous la forme d'un tableau associatif
+            $data = $form->getData();
+            dump($data);
+
+            // Ou récupérer les champs un par un
+            $nom = $form->get("objet")->getData();
+            dd($nom);
+
+            // Envoi de l'e-mail, sauvegarde en base de données, etc.
+
+            // Redirection vers accueil
+            return $this->redirectToRoute('app_accueil');
+        }
+
+        return $this->render('contact/index.html.twig', [
               'form' => $form
         ]);
     }
---
+++ src/Form/DemoFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DemoFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('objet')
+            ->add('email')
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+                'required' => false
+                ]
+            )
+            ->add('save', SubmitType::class, [
+                'label' => 'Envoyer le message'])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Le formulaire n'est associé à aucune entité !!!
+            // 'data_class' => Contact::class,
+        ]);
+    }
+}
