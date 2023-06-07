1495a80ebbf2656f9cae5caf084b25538dcadf8f
Mihaela <mihaela.deaca@gmail.com>
2023-06-07 02:34:56 +0200

    add contactForm

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,28 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Form\ContactFormType;
-use http\Client\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ContactFormType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            //on crée une instance de Contact
+            $message = new Contact();
             // Traitement des données du formulaire
             $data = $form->getData();
+            //on stocke les données récupérées dans la variable $message
+            $message = $data;
+
+            $entityManager->persist($message);
+            $entityManager->flush();
 
             // Envoi de l'e-mail, sauvegarde en base de données, etc.
 
-            // Redirection vers une page de succès
-            return $this->redirectToRoute('success');
+            // Redirection vers accueil
+            return $this->redirectToRoute('app_accueil');
         }
 //A partir de la version 6.2 de Symfony, on n'est plus obligé d'écrire $form->createView(), il suffit de passer l'instance de FormInterface à la méthode render
 
