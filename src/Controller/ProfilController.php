729d58a5fc50e7602c99ff01d73cea51fd16eb27
Mihaela <mihaela.deaca@gmail.com>
2023-06-16 07:29:50 +0200

    auth

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
+    private $userRepo;
+
+    public function __construct(UtilisateurRepository $userRepo){
+        $this->userRepo = $userRepo;
+    }
     #[Route('/profil', name: 'app_profil')]
     public function index(): Response
     {
-        $info = ['lastname' => 'Loper', 'firstname' => 'Dave', 'email' => 'daveloper@code.dom', 'birthdate' => '01/01/1970'];
+        $identifiant = $this->getUser()->getUserIdentifier();
+        if($identifiant){
+            $info = $this->userRepo->findOneBy(["email" =>$identifiant]);
+        }
+//        $info = ['lastname' => 'Loper', 'firstname' => 'Dave', 'email' => 'daveloper@code.dom', 'birthdate' => '01/01/1970'];
 
         return $this->render('profil/index.html.twig', [
             'informations' => $info
