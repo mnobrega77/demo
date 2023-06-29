1b5307b4420524ed94ba39de60888790a84aabe0
@mnobrega <mnobrega77@yahoo.com>
2023-06-29 17:08:20 +0200

    wp

---
+++ src/Controller/ReactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReactController extends AbstractController
+{
+
+    #[Route('/react', name: 'app_react')]
+    public function index(): Response
+    {
+        //on charge juste la vue Twig
+        
+        return $this->render('react/index.html.twig', [
+            'controller_name' => 'ReactController',
+        ]);
+    }
+}
