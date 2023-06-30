3c13e9a625c4f5b15066dc4e5a18627cbb08482f
@mnobrega <mnobrega77@yahoo.com>
2023-06-30 12:25:40 +0200

    api workinpropgress

--- react/my_react_app/jsx/MonPremierComposant.jsx
+++ react/my_react_app/jsx/MonPremierComposant.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Button, Carousel, CarouselItem, Spinner } from "react-bootstrap";
-import he from "he";
+
 
 const MonPremierComposant = (props) => {
     //index du carrousel
@@ -19,7 +19,7 @@ const MonPremierComposant = (props) => {
     //get the 10 last articles from interim-info; déplacée ici le 13/06
     useEffect( () => {
         //call the 'liste_articles' route from AgenceCtrl
-        axios.get("https://reqres.in/api/users").then((response)=>{
+        axios.get("https://reqres.in/api/users?per_page=20").then((response)=>{
             setUsers(response.data.data);
             // console.log(response.data.data);
             setLoading(false);
@@ -30,27 +30,37 @@ const MonPremierComposant = (props) => {
 
     return (
         <div className={"container"}>
-            <Carousel activeIndex={index} onSelect={handleSelect} className={"mt-5"}>
-                {
-                    users && users.map((item, index)=> (
-                        <Carousel.Item className="w-100" key={index}>
-                            <img
-                                className="carouselImage"
-                                src={item.avatar}
-                                alt={"image-article"}
-                                style={{height: "240px"}}
-                            />
-                            <Carousel.Caption>
+            {
+                users ? (
+
+                ) : ()
+            <div className="row">
+                <div className={'col-12 mt-4'}>
+                    <h1 className={'p-3 text-center'} style={{color: "#4169E1", fontSize: "2.8em"}}>Notre équipe</h1>
+                    <hr />
+                </div>
+            </div>
+                <Carousel activeIndex={index} onSelect={handleSelect} className={"mt-5"}>
+                    {
+                        users && users.map((item, index)=> (
+                            <Carousel.Item className="w-100" key={index}>
+                                <img
+                                    className="carouselImage mr-2"
+                                    src={item.avatar}
+                                    alt={"photo-user"}
+                                    style={{height: "240px" }}
+                                />
+                                <Carousel.Caption>
                                 <span className="text-white rounded">
                                     <h2 style={{color: "#4169E1", fontSize: "2.5em", textShadow: "2px 2px 4px #4169E1"}}>{item.first_name} {item.last_name}</h2>
                                     <p style={{color: "#4169E1", fontSize: "2em"}}>{item.email}</p>
                                 </span>
-                            </Carousel.Caption>
-                        </Carousel.Item>)
-                    )
-                }
+                                </Carousel.Caption>
+                            </Carousel.Item>)
+                        )
+                    }
 
-            </Carousel>
+                </Carousel>
         </div>
     );
 
