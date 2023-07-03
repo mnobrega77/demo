0b78e4638af03d34aa3a36366716860e696d57b2
@mnobrega <mnobrega77@yahoo.com>
2023-07-03 17:20:01 +0200

    tt

--- react/my_react_app/jsx/MonPremierComposant.jsx
+++ react/my_react_app/jsx/MonPremierComposant.jsx
@@ -32,24 +32,23 @@ const MonPremierComposant = (props) => {
         <div className={"container"}>
             {
                 users ? (
-
-                ) : ()
-            <div className="row">
-                <div className={'col-12 mt-4'}>
-                    <h1 className={'p-3 text-center'} style={{color: "#4169E1", fontSize: "2.8em"}}>Notre équipe</h1>
-                    <hr />
-                </div>
-            </div>
-                <Carousel activeIndex={index} onSelect={handleSelect} className={"mt-5"}>
-                    {
-                        users && users.map((item, index)=> (
-                            <Carousel.Item className="w-100" key={index}>
-                                <img
-                                    className="carouselImage mr-2"
-                                    src={item.avatar}
-                                    alt={"photo-user"}
-                                    style={{height: "240px" }}
-                                />
+                    <>
+                        <div className="row">
+                            <div className={'col-12 mt-4'}>
+                                <h1 className={'p-3 text-center'} style={{color: "#4169E1", fontSize: "2.8em"}}>Notre équipe</h1>
+                                <hr />
+                            </div>
+                        </div>
+                        <Carousel activeIndex={index} onSelect={handleSelect} className={"mt-5"}>
+                        {
+                            users && users.map((item, index)=> (
+                                <Carousel.Item className="w-100" key={index}>
+                                    <img
+                                        className="carouselImage mr-2"
+                                        src={item.avatar}
+                                        alt={"photo-user"}
+                                        style={{height: "240px" }}
+                                    />
                                 <Carousel.Caption>
                                 <span className="text-white rounded">
                                     <h2 style={{color: "#4169E1", fontSize: "2.5em", textShadow: "2px 2px 4px #4169E1"}}>{item.first_name} {item.last_name}</h2>
@@ -61,6 +60,12 @@ const MonPremierComposant = (props) => {
                     }
 
                 </Carousel>
+                </>
+                ) : (
+                    <Spinner animation="grow" variant="info" />
+                )
+            }
+            
         </div>
     );
 
