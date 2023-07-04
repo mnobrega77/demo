b70bbfd0dc13e7462633293b9bc5c8c071de038f
@mnobrega <mnobrega77@yahoo.com>
2023-07-04 13:27:59 +0200

    react wip

--- react/my_react_app/jsx/MonPremierComposant.jsx
+++ react/my_react_app/jsx/MonPremierComposant.jsx
@@ -1,21 +1,16 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, Carousel, CarouselItem, Spinner } from "react-bootstrap";
+import { Button, Card, CardImg, Spinner } from "react-bootstrap";
 
 
 const MonPremierComposant = (props) => {
-    //index du carrousel
-    const [index,setIndex] = useState(0);
+   
     //etat de l'animation du chargement des donnees
     const [loading,setLoading] = useState(true);
     const [users, setUsers] = useState([]);
 
 
-    //set l'index du carrousel en cas de changement
-    const handleSelect = (selectedIndex,e) => {
-        setIndex(selectedIndex);
-    };
-
+   
     //get the 10 last articles from interim-info; déplacée ici le 13/06
     useEffect( () => {
         //call the 'liste_articles' route from AgenceCtrl
@@ -39,28 +34,26 @@ const MonPremierComposant = (props) => {
                                 <hr />
                             </div>
                         </div>
-                        <Carousel activeIndex={index} onSelect={handleSelect} className={"mt-5"}>
-                        {
+                        <div className="row">
+                            
+                            {
                             users && users.map((item, index)=> (
-                                <Carousel.Item className="w-100" key={index}>
-                                    <img
-                                        className="carouselImage mr-2"
-                                        src={item.avatar}
-                                        alt={"photo-user"}
-                                        style={{height: "240px" }}
-                                    />
-                                <Carousel.Caption>
-                                <span className="text-white rounded">
-                                    <h2 style={{color: "#4169E1", fontSize: "2.5em", textShadow: "2px 2px 4px #4169E1"}}>{item.first_name} {item.last_name}</h2>
-                                    <p style={{color: "#4169E1", fontSize: "2em"}}>{item.email}</p>
-                                </span>
-                                </Carousel.Caption>
-                            </Carousel.Item>)
-                        )
-                    }
-
-                </Carousel>
-                </>
+                                <div className="col-md-4 p-2" key={index}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={item.avatar} />
+                                    <Card.Body>
+                                        <Card.Title>{item.first_name} {item.last_name}</Card.Title>
+                                        <Card.Text>
+                                        {item.email}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>)
+                            )}
+                            
+                            
+                        </div>
+                        </>
                 ) : (
                     <Spinner animation="grow" variant="info" />
                 )
